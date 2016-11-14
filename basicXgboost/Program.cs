263dc25bc0cdd6151cab982e5b520d8ca09841ef
Adam Dobrzycki <adamd@picnet.com.au>
2016-11-14 11:45:06 +1100

    moved dll and test file to better locations

--- basicXgboost/Program.cs
+++ basicXgboost/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System;
+using System.IO;
 using System.Runtime.InteropServices;
 
 namespace basicXgboost
 {
   class Program
   {
-    [DllImport("libxgboost.dll", CharSet = CharSet.Auto)]
+    [DllImport("../../libs/libxgboost.dll", CharSet = CharSet.Auto)]
     public static extern int XGDMatrixCreateFromFile([MarshalAs(UnmanagedType.LPStr)] string file, int silent, IntPtr outputPtr);
 
-    [DllImport("libxgboost.dll", CharSet = CharSet.Auto)]
+    [DllImport("../../libs/libxgboost.dll", CharSet = CharSet.Auto)]
     public static extern int XGDMatrixNumCol(IntPtr dmatrixPtr, IntPtr dmatrixColumnsPtr);
 
     static void Main(string[] args)
@@ -16,7 +17,7 @@ namespace basicXgboost
       IntPtr dmatrixPtr = Marshal.AllocHGlobal(1000000);
       IntPtr dmatrixColumnsPtr = Marshal.AllocHGlobal(10);
 
-      int result = XGDMatrixCreateFromFile("test.txt", 0, dmatrixPtr);
+      int result = XGDMatrixCreateFromFile("../../libs/test.txt", 0, dmatrixPtr);
       int cols = XGDMatrixNumCol(dmatrixPtr, dmatrixColumnsPtr);
 
       Marshal.FreeHGlobal(dmatrixPtr);
