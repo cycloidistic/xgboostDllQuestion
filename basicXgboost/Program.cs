e6516fbc245e4efa4d3e5fdfa580dffaa5413be6
Adam Dobrzycki <adamd@picnet.com.au>
2016-11-14 11:27:25 +1100

    added other files

---
+++ basicXgboost/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace basicXgboost
+{
+  class Program
+  {
+    [DllImport("libxgboost.dll", CharSet = CharSet.Auto)]
+    public static extern int XGDMatrixCreateFromFile([MarshalAs(UnmanagedType.LPStr)] string file, int silent, IntPtr outputPtr);
+
+    [DllImport("libxgboost.dll", CharSet = CharSet.Auto)]
+    public static extern int XGDMatrixNumCol(IntPtr dmatrixPtr, IntPtr dmatrixColumnsPtr);
+
+    static void Main(string[] args)
+    {
+      IntPtr dmatrixPtr = Marshal.AllocHGlobal(1000000);
+      IntPtr dmatrixColumnsPtr = Marshal.AllocHGlobal(10);
+
+      int result = XGDMatrixCreateFromFile("test.txt", 0, dmatrixPtr);
+      int cols = XGDMatrixNumCol(dmatrixPtr, dmatrixColumnsPtr);
+
+      Marshal.FreeHGlobal(dmatrixPtr);
+      Marshal.FreeHGlobal(dmatrixColumnsPtr);
+    }
+  }
+}
